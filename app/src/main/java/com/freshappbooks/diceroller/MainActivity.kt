88cc59000c98e861181e779f2a5e7df29e45b1e7
vjmarcus <vjmarcus1@gmail.com>
2019-12-29 15:57:12 +0300

    App works as expected

--- app/src/main/java/com/freshappbooks/diceroller/MainActivity.kt
+++ app/src/main/java/com/freshappbooks/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.freshappbooks.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -21,9 +22,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText : TextView = findViewById(R.id.result_text)
-        val randomint = Random.nextInt(6) + 1
-        resultText.text = randomint.toString()
+        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
 
     }
 }
